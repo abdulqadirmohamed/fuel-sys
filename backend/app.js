cfbb6d7560dbc2321cfa3b51f942d8db489f1790
abdulqadirmohamed <cajiibxaaji02@gmail.com>
2024-08-14 14:59:35 +0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+// CREATE
+app.post('/insert', (req, res)=>{
+
+})
+
+// READ
+app.get('/getAll', (req, res)=>{
+    res.json({name: 'abdulqadir'})
+})
+
+// UPDATE
+// DELETE
+
+app.listen(3000);
