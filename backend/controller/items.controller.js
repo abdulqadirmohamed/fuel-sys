3d4265b2a5cdac771a456df45dc1eac79dcb46bc
abdulqadirmohamed <cajiibxaaji02@gmail.com>
2024-08-17 11:43:15 +0300

    commit push

--- backend/controller/items.controller.js
+++ backend/controller/items.controller.js
@@ -4,7 +4,7 @@ const itemsController = {
     // Gell all posts
     getAll: async (req, res) => {
         try {
-            const [rows, fields] = await pool.query("select * from items")
+            const [rows, fields] = await pool.query("SELECT * FROM items ORDER BY id DESC")
             res.json({
                 data: rows
             })
@@ -34,9 +34,9 @@ const itemsController = {
     // INSERT POST IN TO DATABASE
     create: async (req, res) => {
         try {
-            const { name, description, open_meter, close_meter, volume, rate, Qty, tank } = req.body
-            const sql = "insert into items (name, description, open_meter, close_meter, volume, rate, Qty, tank) values (?, ?, ?, ?, ?, ?, ?, ?)"
-            const [rows, fields] = await pool.query(sql, [name, description, open_meter, close_meter, volume, rate, Qty, tank])
+            const { name, description, open_meter, close_meter, volume, rate, qty, tank } = req.body
+            const sql = "insert into items (name, description, open_meter, close_meter, volume, rate, qty, tank) values (?, ?, ?, ?, ?, ?, ?, ?)"
+            const [rows, fields] = await pool.query(sql, [name, description, open_meter, close_meter, volume, rate, qty, tank])
             res.json({
                 data: rows
             })
@@ -51,10 +51,10 @@ const itemsController = {
     // Update data
     update: async (req, res) => {
         try {
-            const { name, description, open_meter, close_meter, volume, rate, Qty, tank } = req.body
+            const { name, description, open_meter, close_meter, volume, rate, qty, tank } = req.body
             const { id } = req.params
             const sql = "update items set title = ?, content = ? where id = ?"
-            const [rows, fields] = await pool.query(sql, [name, description, open_meter, close_meter, volume, rate, Qty, tank, id])
+            const [rows, fields] = await pool.query(sql, [name, description, open_meter, close_meter, volume, rate, qty, tank, id])
             res.json({
                 data: rows
             })
